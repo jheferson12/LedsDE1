47c8c5f9b35e3bc93ed1a53c58691e4722d3cc17
Jheferson <jheferduvan07@gmail.com>
2023-05-08 18:06:18 -0500

    Initial commit

---
+++ src/main/java/co/edu/umanizales/ledsde/LedsDeApplication.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.ledsde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedsDeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LedsDeApplication.class, args);
+    }
+
+}
